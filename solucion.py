33de7ac6ce17f7299ff9bd98f46ea6a24bccc1da
ArturoCabb <arturcabbb@outlook.com>
2019-11-18 15:42:11 -0600

    Esto ya fue de lo que proyectó sergio que luego me explico y me ayudó a escribir

--- solucion.py
+++ solucion.py
@@ -4,6 +4,7 @@
 import csv
 
 dic_data = {}
+dic_2 = {}
 
 with open("datos_vuelos.csv") as csv_file:
     csv_reader = csv_file.readlines()
@@ -11,5 +12,26 @@ with open("datos_vuelos.csv") as csv_file:
         string = row.split(",")
         country = string[0][:5]
         departure = string[1][3:5]
-        print(country)
-        print(departure)
+
+        if departure not in dic_data:
+            dic_data[departure] = 1
+        else:
+            dic_data[departure] += 1
+        if departure not in dic_2:
+            dic_2[departure] = {}
+        elif country not in dic_2[departure]:
+            dic_2[departure][country] = 1
+        else:
+            dic_2[departure][country] += 1
+
+average = {}
+file = open("resultados.csv","w+")
+file.write("Mes,Pais,Porcentaje\n")
+for month in dic_data:
+    for country in dic_2[month]:
+        average = dic_2[month][country]/dic_data[month]
+        average = average * 100
+        if average >= 20:
+            average = str(average)
+            file.write(month + "," + country + "," + average + "\n")
+file.close()

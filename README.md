f09786251dfaa74789dcff2050317ec518a9c632
ArturoCabb <arturcabbb@outlook.com>
2019-11-27 22:18:58 -0600

    Inicio de la tarea

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # itesm-tc1033-A01369947
 # Tengo miedo
+#URL de acceso https://github.com/ArturoCabb/itesm-tc1033-A01369947.git
---
+++ solucion_t3.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+#Arturo Caballero Ortega
+
+class Attendants():
+    def __init__(self, *args, **kwargs):
+        return super().__init__(*args, **kwargs)
+    def abrir(self):
+

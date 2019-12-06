58942889a0b55fa8b5627210a998b8d403e4beab
ArturoCabb <arturcabbb@outlook.com>
2019-12-05 19:43:48 -0600

    up

--- AirPort.py
+++ AirPort.py
@@ -96,7 +96,6 @@ class Airport:
             value[_passport] = [_flight, _passport, _class, _seat, _location]
             return value
 
-class ModifyTheDic(Airport):    
         def modify_pilot(self, _passport, _forename, _surname, _date_of_birth, 
                          _country, _gender, _marital_status):
             value = self.pilots.values()
